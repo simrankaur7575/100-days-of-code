65508f4c8523d1b6dfa401b31c55fed324224ce9
simrankaur7575 <simransaini7575@gmail.com>
2020-06-09 22:38:46 +0530

    code

---
+++ Strings/Compare Version Numbers.cpp
@@ -0,0 +1,39 @@
+/*
+QUES - Compare two version numbers version1 and version2.
+
+If version1 > version2 return 1,
+If version1 < version2 return -1,
+otherwise return 0.
+
+*/
+int Solution::compareVersion(string string1, string string2) {
+int i = 0; 
+int j = 0;
+long long n1 = string1.size(); 
+long long n2 = string2.size();
+
+unsigned long long num1 = 0;
+unsigned long long num2 = 0;
+while(i<n1 || j<n2)
+{
+    while(i<n1 && string1[i]!='.'){
+        num1 = num1*10+(string1[i]-'0');
+        i++;
+    }
+    
+    while(j<n2 && string2[j]!='.'){
+        num2 = num2*10+(string2[j]-'0');;
+        j++;
+    }
+    
+    if(num1>num2) return 1;
+    else if(num1 < num2) return -1;
+    
+    num1 = 0;
+    num2 = 0;
+    i++;
+    j++;
+}
+
+return 0;
+}
