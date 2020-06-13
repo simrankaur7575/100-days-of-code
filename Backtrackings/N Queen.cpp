77ddabaf67d9ea4d8bff6a452546bf08d9deb92c
simrankaur7575 <simransaini7575@gmail.com>
2020-06-13 23:10:51 +0530

    code

--- Backtrackings/N Queen.cpp
+++ Backtrackings/N Queen.cpp
@@ -9,36 +9,36 @@ void printBoard(int board[N][N]) {
    }
 }
 bool isValid(int board[N][N], int row, int col) {
-   for (int i = 0; i < col; i++) //check whether there is queen in the left or not
+   for (int i = 0; i < col; i++)
       if (board[row][i])
          return false;
    for (int i=row, j=col; i>=0 && j>=0; i--, j--)
-      if (board[i][j]) //check whether there is queen in the left upper diagonal or not
+      if (board[i][j])
          return false;
    for (int i=row, j=col; j>=0 && i<N; i++, j--)
-      if (board[i][j]) //check whether there is queen in the left lower diagonal or not
+      if (board[i][j])
          return false;
    return true;
 }
 bool solveNQueen(int board[N][N], int col) {
-   if (col >= N) //when N queens are placed successfully
+   if (col >= N)
       return true;
-   for (int i = 0; i < N; i++) { //for each row, check placing of queen is possible or not
+   for (int i = 0; i < N; i++) {
       if (isValid(board, i, col) ) {
-         board[i][col] = 1; //if validate, place the queen at place (i, col)
-         if ( solveNQueen(board, col + 1)) //Go for the other columns recursively
+         board[i][col] = 1;
+         if ( solveNQueen(board, col + 1))
             return true;
-         board[i][col] = 0; //When no place is vacant remove that queen
+         board[i][col] = 0;
       }
    }
-   return false; //when no possible order is found
+   return false;
 }
 bool checkSolution() {
    int board[N][N];
    for(int i = 0; i<N; i++)
    for(int j = 0; j<N; j++)
-   board[i][j] = 0; //set all elements to 0
-   if ( solveNQueen(board, 0) == false ) { //starting from 0th column
+   board[i][j] = 0;
+   if ( solveNQueen(board, 0) == false ) { 
       cout << "Solution does not exist";
       return false;
    }
