b32b109a9f4e4a7f6c50235cbc120399859fee38
simrankaur7575 <simransaini7575@gmail.com>
2020-06-20 22:52:04 +0530

    code

---
+++ Bit Manipulation/Number of 1 Bits.cpp
@@ -0,0 +1,18 @@
+/*
+QUES - Write a function that takes an unsigned integer and returns the number of 1 bits it has.
+
+Example:
+
+The 32-bit integer 11 has binary representation
+
+00000000000000000000000000001011
+*/
+int Solution::numSetBits(unsigned int A) {
+int count=0;
+while(A)
+{
+A=(A)&(A-1);
+count++;
+}
+return count;
+}
