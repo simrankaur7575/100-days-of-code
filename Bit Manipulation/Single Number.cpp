e2f9bf8759899044ebfd1d7ae1826ad3893b8f49
simrankaur7575 <simransaini7575@gmail.com>
2020-06-22 22:51:02 +0530

    code

---
+++ Bit Manipulation/Single Number.cpp
@@ -0,0 +1,13 @@
+/*
+QUES - Given an array of integers, every element appears twice except for one. Find that single one.
+
+Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+int Solution::singleNumber(const vector<int> &A) {
+     int n = A.size();
+       int result = 0;
+       for (int i = 0; i < n; i++) {
+           result ^= A[i];
+       }
+       return result;
+}
