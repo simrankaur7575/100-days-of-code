43f9d3b8a3688b7f3016e47813a950de3f630e73
simrankaur7575 <simransaini7575@gmail.com>
2020-06-24 23:02:04 +0530

    code

--- Two Pointers/Minimize the absolute difference.cpp
+++ Two Pointers/Minimize the absolute difference.cpp
@@ -4,7 +4,6 @@ QUES - Given three sorted arrays A, B and Cof not necessarily same sizes.
 Calculate the minimum absolute difference between the maximum and minimum number from the triplet a, b, c such that a, b, c belongs arrays A, B, C respectively.
 i.e. minimize | max(a,b,c) - min(a,b,c) |.
 
-
 */
 int Solution::solve(vector<int> &A, vector<int> &B, vector<int> &C)
 {
