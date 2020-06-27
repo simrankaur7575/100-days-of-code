21ba01159ca7d17ae23d37aeddb8c5e0d94fb899
simrankaur7575 <simransaini7575@gmail.com>
2020-06-27 22:31:09 +0530

    code

--- Two Pointers/Diffk.cpp
+++ Two Pointers/Diffk.cpp
@@ -1,6 +1,7 @@
 /*
 QUES - Given an array ‘A’ of sorted integers and another non negative integer k, find if there exists 2 indices i and j such that A[i] - A[j] = k, i != j.
 */
+
 int Solution::diffPossible(vector<int> &A, int B) {
  
     for(int i=0;i<A.size()-1;i++)
