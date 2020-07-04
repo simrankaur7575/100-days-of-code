13ee063e84b87116db7b45b0ef53b25ddba55ea2
simrankaur7575 <simransaini7575@gmail.com>
2020-07-04 23:27:34 +0530

    code

--- Hashing/2 Sum.cpp
+++ Hashing/2 Sum.cpp
@@ -6,7 +6,6 @@ Put both these numbers in order in an array and return the array from your funct
 
 If multiple solutions exist, output the one where index2 is minimum. If there are multiple solutions with the minimum index2, choose the one with minimum index1 out of them.
 */
-
 #include <unordered_map>
 vector<int> Solution::twoSum(const vector<int> &num, int target) {
     // map to store the mapping from number to its index. 
