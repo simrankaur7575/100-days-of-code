2ebb34dbd43f2e0ee10b5c8ef1f96e06dff0dbec
simrankaur7575 <simransaini7575@gmail.com>
2020-07-14 22:56:06 +0530

    code

---
+++ Hashing/Substring Concatenation.cpp
@@ -0,0 +1,27 @@
+/*
+QUES - You are given a string, S, and a list of words, L, that are all of the same length.
+
+Find all starting indices of substring(s) in S that is a concatenation of each word in L exactly once and without any intervening characters.
+
+
+*/
+vector<int> Solution::findSubstring(string s, const vector<string> &a){
+vector<int> res;
+int n=s.size();
+int l=(a.size())*(a[0].size());
+int w=a[0].size();
+unordered_map<string,int> m,temp;
+for(auto it:a)
+m[it]++;
+for(int i=0;i<n-l+1;i++){
+for(int j=i;j<i+l;j+=w){
+string x=s.substr(j,w);
+temp[x]++;
+}
+if(m==temp){
+res.push_back(i);
+}
+temp.clear();
+}
+return res;
+}
