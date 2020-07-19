b40d3794cc9ff2524e6d220630e2c68d7930ec43
simrankaur7575 <simransaini7575@gmail.com>
2020-07-19 23:16:14 +0530

    code

---
+++ Heaps And Maps/Magician and Chocolates.cpp
@@ -0,0 +1,25 @@
+/*
+QUES - Given N bags, each bag contains Bi chocolates. There is a kid and a magician. In one unit of time, kid chooses a random bag i, eats Bi chocolates, then the magician fills the ith bag with floor(Bi/2) chocolates.
+
+Find the maximum number of chocolates that kid can eat in A units of time.
+*/
+int Solution::nchoc(int A, vector<int> &B) {
+    
+    std::priority_queue<int> bag_sizes; 
+    
+    for (int initial_size : B) {
+        bag_sizes.push(initial_size);
+    }
+    
+    int result = 0;
+    
+    for (int i = 0; i < A; i++) {
+        int largest = bag_sizes.top();
+        if (largest == 0) { break; }
+        result += largest;
+        bag_sizes.pop();
+        bag_sizes.push(largest / 2);
+    }
+    
+    return result % static_cast<int>(std::pow(10, 9) + 7);
+}
