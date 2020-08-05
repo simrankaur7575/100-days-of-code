3abb9357ba702f1480c726a8b9b27d414cf28dd9
simrankaur7575 <simransaini7575@gmail.com>
2020-08-05 22:55:19 +0530

    code

--- Tree/Diagonal Traversal.cpp
+++ Tree/Diagonal Traversal.cpp
@@ -13,7 +13,6 @@ Given a Binary Tree A containing N nodes, return all diagonal elements in a bina
  * };
  */
 
-
 vector<int> Solution::solve(TreeNode* A) {
     vector<int> v;
     queue<TreeNode *> q;
