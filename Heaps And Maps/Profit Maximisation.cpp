f36d72ff35e2d37989ea2afc46686cb9f198e446
simrankaur7575 <simransaini7575@gmail.com>
2020-08-07 22:18:35 +0530

    code

--- Heaps And Maps/Profit Maximisation.cpp
+++ Heaps And Maps/Profit Maximisation.cpp
@@ -3,6 +3,7 @@ QUES - Given an array A , representing seats in each row of a stadium. You need 
 
 Each seat costs equal to the number of vacant seats in the row it belongs to. The task is to maximize the profit by selling the tickets to B people.
 */
+
 int Solution::solve(vector<int> &A, int B) 
 {
     map<int,int> mp;
