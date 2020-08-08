e435ba04f1fa9f92bbcbc62b43f5bc742730265b
simrankaur7575 <simransaini7575@gmail.com>
2020-08-08 23:06:46 +0530

    code

---
+++ Tree/Inorder Traversal.cpp
@@ -0,0 +1,29 @@
+/*
+QUES - Given a binary tree, return the inorder traversal of its nodes’ values.
+
+*/
+vector<int> Solution::inorderTraversal(TreeNode* A) {
+
+stack<TreeNode *> s;
+vector<int> ans;
+TreeNode* root = A;
+while(true){
+    
+    
+    while(root){
+        s.push(root);
+        root = root->left;
+    }
+    
+    if(s.empty()){
+        break;
+    }
+    
+    root = s.top();
+    s.pop();
+    ans.push_back(root->val);
+    root = root->right;
+    
+}
+return ans;
+}
